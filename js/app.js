f598d6c689c676e2a8ba5facf1ed869fe1654d7d
plaus <plaus@live.unc.edu>
2014-11-16 20:00:33 -0500

    trying differ app js

--- js/app.js
+++ js/app.js
@@ -1,6 +1,11 @@
 /* global $,document,console,Parse */
-var htmlBuilder;
-$(document).ready(function() {
+/*$(document).ready(function() {
+	
+console.log("start page");
+$("#addGeorge").click(function() { 
+
+function addGeorge(){
+	console.log("addGeorge()");
 	
 	var parseAPPID = "W91Wk1zFsAuDszjC07tMNEaatOf4cM1DS5m2k03P";
 	var parseJSID = "rReOROOQgAkOePRoPa8TJ9UavsqAi2za36LOGIfh";
@@ -10,10 +15,9 @@ $(document).ready(function() {
 var findGeorge = Parse.Object.extend("findGeorge");
 var htmlBuilder=[];
 
-	getGeorge(findGeorge);
+	//getGeorge(findGeorge);
 
-	$("#addGeorge").on("#submit", function(e) {
-		e.preventDefault();
+	
 
 		console.log("Handling the submit");
 		//add error handling here
@@ -32,12 +36,44 @@ var htmlBuilder=[];
 			error:function(e) {
 				console.dir(e);
 			}
-		});
+			
+	function getGeorge(findGeorge){
+	console.log("getGeorge" + findGeorge);
+	var query = new Parse.Query(findGeorge);
+	query.find({
+		success: function(results) {
+			console.log(results);
+			$.each(results, function(index, value) {
+				console.log(results[index].attributes.serialNumber);
+				htmlBuilder += 
+    "<tr>" +
+      "<td>" + results[index].attributes.serialNumber + "</td>" +
+      "<td>" + "</td>" +
+    "</tr>" 
+
+			});
+			$("#serialNumber").html(htmlBuilder);
+		},
+
+		error: function(error) {
+
+		}
+	});
+}
 		
 	});
+
+}
+
+	return false; });
 	
-});
-var htmlBuilder;
+});*/
+
+
+
+
+
+/*var htmlBuilder;
 document.addEventListener("deviceready", getGeorge, false);
 
 	
@@ -116,5 +152,129 @@ Parse.Cloud.beforeSave("Post", function(request, response) {
     post.set("hashtags", hashtags);
     response.success();
 });
+*/
 
+var lat;
+var long;
+var myLocation;
 
+function displayContent(){
+var parseAPPID = "W91Wk1zFsAuDszjC07tMNEaatOf4cM1DS5m2k03P";
+var parseJSID = "rReOROOQgAkOePRoPa8TJ9UavsqAi2za36LOGIfh";
+//Initialize Parse
+Parse.initialize(parseAPPID,parseJSID);
+var NoteOb = Parse.Object.extend("photos");
+var query = new Parse.Query(NoteOb);
+query.withinMiles("geopoint", myLocation, 10);
+var yesterday = new Date();
+yesterday.setDate(yesterday.getDate()-1);
+query.greaterThan("createdAt", yesterday);
+query.descending("createdAt");
+query.find({
+success:function(results) {
+var s = "";
+for(var i=0; i<results.length; i++) {
+//Lame - should be using a template
+s += "<div class='row'> <div class='large-12 columns'> <div class='callout panel'>";
+var pic = results[i].get("picture");
+if(pic) {
+s += "<br/><img src='" + pic.url() + "'>";
+}
+s += results[i].get("text");
+s += "</div> </div> </div>"
+}
+$("#content").html(s);
+},error:function(e) {
+}
+});
+}
+function onDeviceReady() {
+console.log("onDeviceReady()");
+var imagedata = "";
+navigator.geolocation.getCurrentPosition(gotGeo, errorGeo,{enableHighAccuracy: true, maximumAge: 5000, timeout: 5000 });
+}
+function gotGeo(position){
+lat=position.coords.latitude;
+long=position.coords.longitude;
+console.log(lat);
+console.log(long);
+myLocation = new Parse.GeoPoint({latitude: lat, longitude: long});
+displayContent();
+}
+function errorGeo(error){
+alert('code: ' + error.code + '\n' +
+'message: ' + error.message + '\n');
+}
+function submitBtn(){
+$('#spinner').show();
+var parseAPPID = "sqjjNOSioMoqfwC5aEw4OAoJsPCF1hbWeBLSKB59";
+var parseJSID = "EQZJbB4ZeutL6IeyJP5NN2ZHXCgp0ml920CDilX9";
+//Initialize Parse
+Parse.initialize(parseAPPID,parseJSID);
+var NoteOb = Parse.Object.extend("photos");
+var caption = $("#caption").val();
+/*
+Take care of saving the image
+*/
+if(imagedata != "") {
+var parseFile = new Parse.File("mypic.jpg", {base64:imagedata});
+var point = new Parse.GeoPoint(lat, long);
+console.log(parseFile);
+parseFile.save().then(function() {
+var note = new NoteOb();
+note.set("text",caption);
+note.set("picture",parseFile);
+note.set("geopoint", point);
+note.save(null, {
+success:function(ob) {
+$('#spinner').hide();
+window.location.href = "index.html";
+}, error:function(e) {
+console.log("Oh crap", e);
+}
+});
+cleanUp();
+}, function(error) {
+console.log("Error");
+console.log(error);
+});
+} else {
+var note = new NoteOb();
+note.set("text",caption);
+note.save(null, {
+success:function(ob) {
+$.mobile.changePage("index.html");
+}, error:function(e) {
+console.log("Oh crap", e);
+}
+});
+cleanUp();
+}
+}
+function capturePhoto(){
+//alert("capture button working");
+navigator.camera.getPicture(gotPic,failHandler,{quality:30, destinationType:0 });
+}
+function choosePhoto(){
+//alert("capture button working");
+navigator.camera.getPicture(gotPic,failHandler,{sourceType:0, destinationType:0, quality:30});
+}
+function gotPic(data) {
+console.log('got here');
+imagedata = data;
+//$("#takePicBtn").text("Picture Taken!").button("refresh");
+}
+function failHandler(e) {
+alert("ErrorFromC");
+alert(e);
+console.log(e.toString());
+}
+function cleanUp() {
+imagedata = "";
+//$("#submitBtn").removeAttr("disabled").button("refresh");
+$("#caption").val("");
+//$("#takePicBtn").text("Add Pic").button("refresh");
+}
+//});
+function okay(message) {
+}
